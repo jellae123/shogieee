21fa6a14f11a0d2749b1aae4af77dd7d6038452b
jellae123 <estradajellarose@gmail.com>
2025-01-10 13:49:49 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('changeButton').addEventListener('click', function() {
+    // Change the image
+    document.getElementById('image').src = 'https://www.science.org/do/10.1126/science.abi5787/abs/main_puppies_1280p.jpg';
+
+    // Change the text
+    document.getElementById('text').textContent = "waaaaaaah raaap";
+});
